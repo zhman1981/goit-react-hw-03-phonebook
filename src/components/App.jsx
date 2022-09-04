d45a6b935c4ddfbc1fdd572213a5462da061c1cd
zhman1981 <zhman@ukr.net>
2022-09-04 13:33:36 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,19 @@ import { css } from '@emotion/css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    if (JSON.parse(localStorage.getItem('contacts'))) {
+      this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   onFormResponse = data => {
     for (const contact of this.state.contacts) {
